4dbba854ca5270260dc15c23272a9957c14417f1
Ankit Raj <ankitrajk27@gmail.com>
2022-05-21 21:29:15 +0530

    button

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-let randomNumber1 = Math.floor((Math.random()*6 + 1));
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
 let randomDiceImage1 = "dice" + randomNumber1 + ".png";
-let randomeImageSource1 = "images/"+ randomDiceImage1;
+let randomeImageSource1 = "images/" + randomDiceImage1;
 
 document.querySelectorAll("img")[0].setAttribute("src", randomeImageSource1);
 
-let randomNumber2 = Math.floor((Math.random()*6 + 1));
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 let randomDiceImage2 = "dice" + randomNumber2 + ".png";
-let randomeImageSource2 = "images/"+ randomDiceImage2;
+let randomeImageSource2 = "images/" + randomDiceImage2;
 
 document.querySelectorAll("img")[1].setAttribute("src", randomeImageSource2);
 
+// if(randomNumber1>randomNumber2)
+// {
+//     document.querySelector("h1").innerHTML= "Player 1 Wins!"
+// }
+// else if(randomNumber2>randomNumber1)
+// {
+//     document.querySelector("h1").innerHTML="Player 2 Wins!"
+// }
+// else{
+//     document.querySelector("h1").innerHTML="Draw! Roll Again"
+// }
+document.querySelector('h1').innerHTML="Player 1's turn!";
+function togg()
+{
+    
+    let text = document.querySelector("h1").innerHTML;
+    // if(text.innerHTML==="Player 1's turn!")
+    // {
+    //     text.innerHTML = "Player 2's turn!";
+    // }
 
-if(randomNumber1>randomNumber2)
-{
-    document.querySelector("h1").innerHTML="Player 1 Wins!";
-}
-else if(randomNumber2>randomNumber1)
-{
-    document.querySelector("h1").innerHTML="Player 2 Wins!";
-}
-else{
-    document.querySelector("h1").innerHTML="Draw!";
+    document.querySelector("h1").innerHTML = text == "Player 1's turn!" ? "Player 2's turn!" : "Player 1's turn!";
+    // else
+    // {
+    //     text.innerHTML="Player 1's turn!";
+    // }
 }
