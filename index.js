416b6b7a05824d8f4d893d0cdb52448dd0d3d850
Ankit Raj <ankitrajk27@gmail.com>
2022-05-21 15:53:37 +0530

    Dice Roll Game

---
+++ index.js
@@ -0,0 +1,24 @@
+let randomNumber1 = Math.floor((Math.random()*6 + 1));
+let randomDiceImage1 = "dice" + randomNumber1 + ".png";
+let randomeImageSource1 = "images/"+ randomDiceImage1;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomeImageSource1);
+
+let randomNumber2 = Math.floor((Math.random()*6 + 1));
+let randomDiceImage2 = "dice" + randomNumber2 + ".png";
+let randomeImageSource2 = "images/"+ randomDiceImage2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomeImageSource2);
+
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+}
+else if(randomNumber2>randomNumber1)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
